4235eab88f325a0ada3274616470efb04aa27a52
Srndo <xsesta06@stud.fit.vutbr.cz>
2021-08-11 12:42:57 +0200

    Detail Storyboard constraints

---
+++ DemoApp/Table/Detail/DetailModel.swift
@@ -0,0 +1,31 @@
+//
+//  DetailModel.swift
+//  DemoApp
+//
+//  Created by Simon Sestak on 11/08/2021.
+//
+
+import Foundation
+
+struct DetailUserModel: Decodable {
+    let name: String
+    let email: String
+    let phone: String
+    let address: Address
+
+    init(_ user: User?) {
+        self.name = user?.name ?? "asd"
+        self.email = user?.email ?? ""
+        self.phone = user?.phone ?? ""
+        let geo = GeoLocation(lat: "", lng: "")
+        self.address = Address(street: "", suite: "", city: "", zipcode: "", geo: geo)
+    }
+
+    init(_ cellUser: CellUserModel) {
+        self.name = cellUser.name
+        self.email = ""
+        self.phone = ""
+        let geo = GeoLocation(lat: "", lng: "")
+        self.address = Address(street: "", suite: "", city: "", zipcode: "", geo: geo)
+    }
+}
--- DemoApp/Table/Detail/DetailViewModel.swift
+++ DemoApp/Table/Detail/DetailViewModel.swift
@@ -38,26 +38,3 @@ class DetailViewModel {
         coordinator.didFinish()
     }
 }
-
-struct DetailUserModel: Decodable {
-    let name: String
-    let email: String
-    let phone: String
-    let address: Address
-
-    init(_ user: User?) {
-        self.name = user?.name ?? "asd"
-        self.email = user?.email ?? ""
-        self.phone = user?.phone ?? ""
-        let geo = GeoLocation(lat: "", lng: "")
-        self.address = Address(street: "", suite: "", city: "", zipcode: "", geo: geo)
-    }
-
-    init(_ cellUser: CellUserModel) {
-        self.name = cellUser.name
-        self.email = ""
-        self.phone = ""
-        let geo = GeoLocation(lat: "", lng: "")
-        self.address = Address(street: "", suite: "", city: "", zipcode: "", geo: geo)
-    }
-}
