665fbfc01c514723fd20fa3e2f626c840ecb28cc
Srndo <xsesta06@stud.fit.vutbr.cz>
2021-08-19 09:53:53 +0200

    MainVC inherit from UITabBarDelegate

--- DemoApp/Main/MainViewController.swift
+++ DemoApp/Main/MainViewController.swift
@@ -11,9 +11,12 @@ class MainViewController: BaseViewController<MainViewModel>, Storyboarded {
 
     @IBOutlet weak var button1: UIButton?
     @IBOutlet weak var button2: UIButton?
+    @IBOutlet weak var tabBar: UITabBar!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tabBar.delegate = self
+        setTabBarItems()
         button1?.setTitle(viewModel.titleButton1, for: .normal)
         button2?.setTitle(viewModel.titleButton2, for: .normal)
     }
@@ -24,5 +27,22 @@ class MainViewController: BaseViewController<MainViewModel>, Storyboarded {
 
     @IBAction func button2Tap(_ sender: Any) {
         viewModel.button2Tapped()
+    }
+}
+
+extension MainViewController: UITabBarDelegate {
+    private func setTabBarItems() {
+        tabBar.items = viewModel.tabBarItems
+    }
+
+    func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        switch item.tag {
+        case 0:
+            viewModel.button2Tapped()
+        case 1:
+            viewModel.button1Tapped()
+        default:
+            print(item.tag)
+        }
     }
 }
